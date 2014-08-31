36bd6336be04c8f75ff6e60cd882296df6fc6ef4
Konst Kolesnichenko <kolesnichenko@gmail.com>
2014-08-31 15:40:51 +0300

    * much better contour removal based on a mask erosion

--- unshred/features/lines.py
+++ unshred/features/lines.py
@@ -14,24 +14,30 @@ class LinesFeatures(AbstractShredFeature):
         tags = []
         params = {}
 
+        _, _, _, mask = cv2.split(shred)
+
+        # expanding mask for future removal of the contour
+        mask = cv2.morphologyEx(mask, cv2.MORPH_ERODE, (5, 5), iterations=200)
+
         gray = cv2.cvtColor(shred, cv2.COLOR_BGR2GRAY)
         gray_blur = cv2.GaussianBlur(gray, (15, 15), 0)
         edges = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
             cv2.THRESH_BINARY_INV, 5, 1)
         # removing contours from the edges (by drawing them black)
-        cv2.drawContours(edges, contour, -1, (0, 0, 0), 24)
+        edges = edges & mask
         edges = cv2.morphologyEx(edges, cv2.MORPH_ERODE, (6, 6), iterations=2)
 
         # const: uncomment for debug
-        # cv2.imwrite('../debug/edges_%s.png'%name, edges)
+        cv2.imwrite('../debug/edges_%s.png'%name, edges)
+        cv2.imwrite('../debug/mask_%s.png'%name, mask)
 
         lines = cv2.HoughLinesP(edges, 1, numpy.pi/180, 20, minLineLength = 30, maxLineGap = 10)
         if not lines is None:
             # const: uncomment for debug
             #debug images
-            # for x1,y1,x2,y2 in lines[0]:
-                # cv2.line(gray,(x1,y1),(x2,y2),(255,0,0),2)
-            # cv2.imwrite('../debug/houghlines_%s.png'%name, gray)
+            for x1,y1,x2,y2 in lines[0]:
+                cv2.line(gray,(x1,y1),(x2,y2),(255,0,0),2)
+            cv2.imwrite('../debug/houghlines_%s.png'%name, gray)
 
             params['Lines Count'] = len(lines[0])
             tags.append(self.TAG_HAS_LINES_FEATURE)
